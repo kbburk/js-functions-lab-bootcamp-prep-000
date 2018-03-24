203ce195ed8c63e7ef7a2e63f0c335419b8dd449
Kimberly Sampson <kbburk54@yahoo.com>
2018-03-24 06:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function holidayCountdown(holiday, days){
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function happyHolidaysTo(holiday, name){
