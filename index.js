8beb8a13de2329e34c22a2ab33e49414ed2dd656
Kimberly Sampson <kbburk54@yahoo.com>
2018-03-24 04:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function say(greeting, firstName) {
+      return `${greeting}, ${firstName}!`
+}  
+
+function holidayCountDown(holiday, days){
+  return `It's ${days} days until ${holday}!`
+}
